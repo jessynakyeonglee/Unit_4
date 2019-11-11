bf911912200a726c8a00469fa995042f5bcc330c
leena <leena@ssfs.org>
2019-11-11 12:05:53 -0500

    11/11/19

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,5 +1,24 @@
 public class WhileLoops {
     public static void fromHereToThere(int a, int b){
-        while (a<b)
+        if (a>b){
+            System.out.println("Invalid input");
+        }
+        while (a<=b){
+            System.out.println(a);
+            a+=1;
+        }
+    }
+    public static void factors(int c){
+        int num = 0;
+        while (num<=c){
+            num+=1;
+            if (c%num==0){
+                System.out.println(c/num);
+            }
+        }
+    }
+    public static void main(String[]args){
+        fromHereToThere(20,30);
+        factors(12);
     }
 }
