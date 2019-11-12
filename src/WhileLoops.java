ed76a69489f4c66ce47e692da132a1c7d854bebf
leena <leena@ssfs.org>
2019-11-12 14:15:12 -0500

    11/12/19

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class WhileLoops {
+    public static Scanner scan = new Scanner(System.in);
     public static void fromHereToThere(int a, int b){
         if (a>b){
             System.out.println("Invalid input");
@@ -17,8 +20,31 @@ public class WhileLoops {
             }
         }
     }
+    public static String countPosAndNeg(){
+
+        int pcount = 0;
+        int ncount = 0;
+        int num = scan.nextInt();
+        while (num!=0){
+            if (num>0){
+            pcount+=1;
+            }
+            if (num<0) {
+                ncount+=1;
+            }
+            num = scan.nextInt();
+        }
+        return "There were "+pcount+" positive and "+ncount+" negative numbers.";
+    }
+    public static void findMinAndMax(){
+        int num = scan.nextInt();
+
+
+    }
     public static void main(String[]args){
         fromHereToThere(20,30);
         factors(12);
+        System.out.println("Enter a positive or negative number or 0 to quit:");
+        System.out.println(countPosAndNeg());
     }
 }
