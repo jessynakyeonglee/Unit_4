02d3e4d16e94a8d702ca3710c9af6b9d6be36797
leena <leena@ssfs.org>
2019-11-13 14:44:06 -0500

    11/13/19

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -36,15 +36,53 @@ public class WhileLoops {
         }
         return "There were "+pcount+" positive and "+ncount+" negative numbers.";
     }
-    public static void findMinAndMax(){
-        int num = scan.nextInt();
-
-
+    public static String findMinAndMax(){
+        int counter = 0;
+        int min =Integer.MAX_VALUE;
+        int max =Integer.MIN_VALUE;
+        while (counter<5){
+            System.out.print("num: ");
+            int num = scan.nextInt();
+            counter+=1;
+            if (num<min){
+                min=num;
+            }
+            if (num>max){
+                max=num;
+            }
+        }
+        return "Max value is:"+max+"Min value is "+min;
+    }
+    public static Double gradePoint() {
+        int counter = 0;
+        double total =0;
+        System.out.println("Enter seven letter grades (A, B, C, D, or F)");
+        while (counter < 7) {
+            String grade = scan.next();
+            counter += 1;
+            if (grade=="A") {
+                total += 4.0;
+            }
+            else if (grade=="B") {
+                total += 3.0;
+            }
+            else if (grade=="C") {
+                total += 2.0;
+            }
+            else if (grade=="D") {
+                total += 1.0;
+            }
+             else  {
+                total += 0.0;
+            }
+             return (total/7+0.5)*10;
+        }
     }
     public static void main(String[]args){
-        fromHereToThere(20,30);
-        factors(12);
-        System.out.println("Enter a positive or negative number or 0 to quit:");
-        System.out.println(countPosAndNeg());
+        //fromHereToThere(20,30);
+        //factors(12);
+        //System.out.println("Enter a positive or negative number or 0 to quit:");
+        //System.out.println(countPosAndNeg());
+        System.out.println(findMinAndMax());
     }
 }
