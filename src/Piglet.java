edda5b16fb6874ed3776a7c9dcb8121baeceeb68
leena <leena@ssfs.org>
2019-11-19 11:20:18 -0500

    11/19/19

--- src/Piglet.java
+++ src/Piglet.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
 public class Piglet {
-    public static String Rolldice() {
-        int dice = (int) (Math.random() * 6 + 1);
+    public static void Rolldice() {
+        int dice = 0;
         int score = 0;
         Scanner scan = new Scanner(System.in);
-        String choice = scan.next();
-
-
-        while (dice != 1) {
-            score += dice;
-            return "You rolled a " + dice + "! \nRoll again?";
-            if (choice.equals("no")) {
-                return "You got " + score + "points.";
+        while (true) {
+            dice = (int) (Math.random() * 6 + 1);
+            if (dice != 1) {
+                score += dice;
+                System.out.print("You rolled a " + dice + "\nRoll again?");
             }
-            else
+            if (scan.next().equals("no")) {
+                System.out.println("You got " + score + " points");
+            }
+            else if (dice == 1) {
+                System.out.print("You got 0 points!");
+                break;
+            }
 
         }
-
     }
-    public static void main (String[]args){
 
+
+
+
+    public static void main (String[]args){
         System.out.println("Welcome to Piglet!");
-        System.out.println(Rolldice());
+        Rolldice();
     }
 }
+
